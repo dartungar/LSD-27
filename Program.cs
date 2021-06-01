aea7d799c69b4a79278444add4f64fc4b33a7211
danila nikolayev <d.nikolayev@treolan.ru>
2021-06-01 11:22:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LSD_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
